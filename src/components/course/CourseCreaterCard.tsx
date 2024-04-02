5139f64b78b670e6208ad5edba373ab87f3131bc
FrederkBN <fbn212@hotmail.dk>
2024-04-02 08:47:59 +0200

    Make plus-sign centered

--- src/components/course/CourseCreaterCard.tsx
+++ src/components/course/CourseCreaterCard.tsx
@@ -5,20 +5,20 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
 import ClickableCard from "../ui/clickableCard";
 
 const CourseCreatorCard = () => {
   return (
     <ClickableCard href={"/courses/add"}>
-        <Card className="h-[140px] p-4 shadow-md rounded-md cursor-pointer hover:bg-slate-600 flex items-center justify-center">
-          <div className="plus-sign bg-slate-500 text-white rounded-full w-16 h-16 items-center text-center justify-center p-2">
-            <span className="text-3xl font-bold">+</span>
-          </div>
-        </Card>
+      <Card className="flex h-[140px] cursor-pointer items-center justify-center rounded-md p-4 shadow-md hover:bg-slate-600">
+        <div className="plus-sign h-16 w-16 items-center justify-center rounded-full bg-slate-500 p-3 text-center text-white">
+          <span className="text-3xl font-bold">+</span>
+        </div>
+      </Card>
     </ClickableCard>
-  )
-}
+  );
+};
 
-export default CourseCreatorCard
+export default CourseCreatorCard;
