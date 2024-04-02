fd8d4dd0c2ed9e09555857891f1ce3eb210421dd
FrederkBN <fbn212@hotmail.dk>
2024-04-02 08:52:18 +0200

    Fix prefer-const error

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -7,16 +7,22 @@ import CourseCard from "@/components/course/CourseCard";
 import CourseCreatorCard from "@/components/course/CourseCreaterCard";
 
 export default async function Courses() {
-  const courses = await api.get.getCourses()
+  const courses = await api.get.getCourses();
 
-  let courseElements = courses.map(function(course) {
-    return <CourseCard key={course.id} nameShort={course.nameShort} name={course.name}/>;
+  const courseElements = courses.map(function (course) {
+    return (
+      <CourseCard
+        key={course.id}
+        nameShort={course.nameShort}
+        name={course.name}
+      />
+    );
   });
 
   return (
     <div className="grid grid-cols-3 gap-4">
       {courseElements}
-      <CourseCreatorCard/>
+      <CourseCreatorCard />
     </div>
   );
 }
