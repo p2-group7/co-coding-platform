79570ceb1f7ccca7d90f181578a8f4c953f9b348
omarsabbagh0409 <159829357+OmSa120224@users.noreply.github.com>
2024-04-04 12:00:29 +0200

    c

--- src/app/courses/[course]/lectures/[lectureId]/exercises/page.tsx
+++ src/app/courses/[course]/lectures/[lectureId]/exercises/page.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import ExerciseInfoBox from "@/components/codeSpace/exerciseInfoBox";
+import TestSuite from "@/components/codeSpace/testCase";
 
 const App: React.FC = () => {
   return (
     <div>
-      <div
-        className="container mx-auto px-1"
-        style={{ width: "300px", height: "300px" }}
-      >
-        <h1>Programming Exercises</h1>
+      <div>
         <ExerciseInfoBox
           ExsTitle="Exercise 1: Hello World"
           ExsDescription="Write a program that prints 'Hello, World!' to the console."
         />
       </div>
+
     </div>
   );
 };
+
 
 export default App;
--- src/components/codeSpace/exerciseInfoBox.tsx
+++ src/components/codeSpace/exerciseInfoBox.tsx
@@ -7,9 +7,11 @@ interface ExerciseProps {
 
 const ExerciseInfoBox: React.FC<ExerciseProps> = ({ ExsTitle, ExsDescription }) => {
   return (
-    <div className="exercise">
-      <h2>{ExsTitle}</h2>
-      <p>{ExsDescription}</p>
+    <div className="container mx-auto md:w-1/3">
+      <div className="border border-transparent rounded-lg p-4 mb-4 bg-secondary">
+        <h2 className='font-bold'>{ExsTitle}</h2>
+        <p>{ExsDescription}</p>
+      </div>
     </div>
   );
 };
