f7ea00e55638b778d6581dc174dbf0f004520efa
omarsabbagh0409 <159829357+OmSa120224@users.noreply.github.com>
2024-04-07 01:57:24 +0200

    added a edit buttun that has no functionality

--- src/app/courses/[course]/lectures/[lectureId]/exercises/[exerciseId]/page.tsx
+++ src/app/courses/[course]/lectures/[lectureId]/exercises/[exerciseId]/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { api } from "@/trpc/server";
 import ExerciseInfoBox from "@/components/codeSpace/exerciseInfoBox";
+import { Button } from "@/components/ui/button";
+import { Combobox } from "@/components/ui/combobox";
+
 export default async function page({
   params,
 }: {
--- src/components/codeSpace/exerciseInfoBox.tsx
+++ src/components/codeSpace/exerciseInfoBox.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Combobox } from "@/components/ui/combobox";
+import { Combobox } from "../ui/combobox";
+import { Button } from "../ui/button";
 
 export type ExerciseProps = {
   ExsTitle: string;
@@ -12,8 +13,11 @@ const ExerciseInfoBox: React.FC<ExerciseProps> = ({
 }) => {
   return (
     <div className="h-full w-full md:w-1/3">
-      <div className="m-4 mb-4 h-full rounded-lg border border-transparent bg-secondary p-4">
-        <Combobox /> {/* Render the Combobox component */}
+      <div className="m-4 mb-4 flex h-full flex-col rounded-lg border border-transparent bg-secondary p-4">
+        <div className="mb-4 flex items-start">
+          <Combobox />
+          <Button className="mx-5 bg-black">✏️</Button>
+        </div>
         <h2 className="font-bold">{ExsTitle}</h2>
         <p>{ExsDescription}</p>
       </div>
