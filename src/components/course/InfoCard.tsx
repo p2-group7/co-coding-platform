f36040b4efa8c7cc3062cabbe9146bc1be26187c
FrederkBN <fbn212@hotmail.dk>
2024-04-23 13:58:05 +0200

    Add edit button to infocard

--- src/components/course/InfoCard.tsx
+++ src/components/course/InfoCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardDescription,
@@ -6,6 +8,9 @@ import {
 } from "@/components/ui/card";
 
 import ClickableCard from "../ui/clickableCard";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+import { GearIcon } from "@radix-ui/react-icons";
 
 type InfoCardProps = {
   href: string;
@@ -14,12 +19,28 @@ type InfoCardProps = {
 };
 
 const InfoCard: React.FC<InfoCardProps> = ({ href, nameShort, name }) => {
+  const router = useRouter();
+  const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    router.push(href + "/edit");
+  };
   return (
     <ClickableCard href={href}>
       <Card className="h-[140px] cursor-pointer rounded-md p-4 shadow-md hover:bg-secondary">
         <CardHeader>
-          <CardTitle>{name}</CardTitle>
-          <CardDescription className="truncate">{nameShort}</CardDescription>
+          <div className="flex flex-row">
+            <div className="flex flex-1 flex-col">
+              <CardTitle>{name}</CardTitle>
+              <CardDescription className="truncate">
+                {nameShort}
+              </CardDescription>
+            </div>
+            <Button onClick={handleButtonClick}>
+              <GearIcon />
+            </Button>
+          </div>
         </CardHeader>
       </Card>
     </ClickableCard>
