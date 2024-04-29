34521db53483c6a12fa67018b9d7bdc2b7a4e4d5
Tea <NP66QA@student.aau.dk>
2024-04-30 00:04:57 +0200

    added the text box toggle, can edit but no interraction w database yet

--- src/components/course/DescriptionEditor.tsx
+++ src/components/course/DescriptionEditor.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import React, { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+
 
 const schema = z.object({
   text: z.string()
@@ -37,15 +40,17 @@ const EditDescript = () => {
           <input
             {...register("text")}
             defaultValue={text}
+            className="text-white-700 rounded-lg border-2 border-secondary bg-secondary bg-gradient-to-b from-muted/50 to-muted p-6 text-lg no-underline outline-none focus:shadow-md"
           />
           {errors.text && <p>{errors.text.message}</p>}
-          <button type="submit">Save</button>
-          <button type="button" onClick={toggleEditMode}>Cancel</button>
+          <br />
+          <button type="submit" className="flex-row cursor-pointer items-center justify-center rounded-md m-2 p-2 shadow-md hover:bg-secondary">Save</button>
+          <button type="button" className="flex-row cursor-pointer items-center justify-center rounded-md m-2 p-2 shadow-md hover:bg-secondary" onClick={toggleEditMode}>Cancel</button>
         </form>
       ) : (
         <div>
-          <p>{text}</p>
-          <button type="button" onClick={toggleEditMode}>Edit</button>
+          <p className="text-white-700 rounded-lg border-2 border-secondary bg-secondary bg-gradient-to-b from-muted/50 to-muted p-6 text-lg no-underline outline-none focus:shadow-md">{text}</p>
+          <button type="button" className="flex cursor-pointer items-center justify-center rounded-md m-2 p-2 shadow-md hover:bg-secondary" onClick={toggleEditMode}>Edit</button>
         </div>
       )}
     </div>
